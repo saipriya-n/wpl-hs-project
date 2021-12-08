01a5074e9ae0b250e56112343334d4dcfcdcc452
saipriya-n <46355241+saipriya-n@users.noreply.github.com>
2021-12-08 17:59:42 -0600

    final_changes

--- routes/useritems.js
+++ routes/useritems.js
@@ -27,4 +27,45 @@ router.get('/:id', function(req, res, next) {
 });
 
 
+router.post('/filter', function(req,res) {    
+    console.log(req.body.search);    
+    console.log(req.body.filter);    
+    if(req.body.search == "" && req.body.filter == "All" ) {      
+        collection.find({},function(err,items){        
+            if (err) throw err;        
+            res.render('viewall_users', {results: items, user: req.user})      }
+            );    }   
+             else if(req.body.filter == "All") {        
+                 collection.find({},function(err, items){          
+                     if (err) throw err;          
+                     var result = []          
+                     for(i=0;i<items.length;i++) {            
+                         if(items[i].title.toLowerCase().includes(req.body.search.toLowerCase())){              
+                             result.push(items[i])            
+                            }          
+                        }          
+                        res.render('viewall_users', {results: result, user: req.user})      
+                    });      
+                }      else if(req.body.search == "") 
+                {      collection.find({"category":req.body.filter},function(err,items){        
+                    if (err) throw err;        
+                    res.render('viewall_users', {results: items, user: req.user})      
+                });    
+            }          else {      
+                collection.find({"category":req.body.filter},function(err,items){        
+                    if (err) throw err;        
+                    var result = []        
+                    for(i=0;i<items.length;i++) {          
+                        if(items[i].title.toLowerCase().includes(req.body.search.toLowerCase())){            
+                            result.push(items[i])          
+                        }         
+                     }        
+                     res.render('viewall_users', {results: result, user: req.user})      
+                    });    
+                }  
+            });
+
+
+
+
 module.exports = router;
